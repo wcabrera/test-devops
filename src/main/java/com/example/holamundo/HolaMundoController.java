efe60078f4247d14ee9519ceedc81c30926ca087
William Cabrera <wacabrera08@gmail.com>
2026-05-23 10:09:22 -0500

    Config profile

--- src/main/java/com/example/holamundo/HolaMundoController.java
+++ src/main/java/com/example/holamundo/HolaMundoController.java
@@ -1,5 +1,6 @@
 package com.example.holamundo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HolaMundoController {
 
+    @Value("${app.environment}")
+    private String environment;
+
     @GetMapping("/hola")
     public String holaMundo() {
-        return "Hola Mundo!";
+        return "Hola Mundo, soy el ambiente " + environment;
     }
 
     @GetMapping("/hola/nombre")
     public String holaNombre(@RequestParam(defaultValue = "Mundo") String nombre) {
-        return "Hola, " + nombre + "!";
+        return "Hola, " + nombre + "! Soy el ambiente " + environment;
     }
 }
